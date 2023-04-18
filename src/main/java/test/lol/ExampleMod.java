5e8815679aa13e5bc4f76c640eb5f2661f07e9b0
ModxStudios <65139923+ModxStudios@users.noreply.github.com>
2023-04-18 21:16:04 +0000

    Pending changes exported from your codespace

--- src/main/java/test/lol/ExampleMod.java
+++ src/main/java/test/lol/ExampleMod.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
-import net.minecraft.entity.damage.DamageScaling;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
---
+++ src/main/java/test/lol/items/packnite_pickaxeitem.java
@@ -0,0 +1,12 @@
+package test.lol.items;
+
+import net.fabricmc.fabric.api.client.networking.v1.C2SPlayChannelEvents.Register;
+import net.minecraft.item.PickaxeItem;
+
+public class packnite_pickaxe extends PickaxeItem {
+    public packnite_pickaxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+
+        
+    }
+}
